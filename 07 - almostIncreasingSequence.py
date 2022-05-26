ad7f1dfb380ac3032db4c2057ba353b40a459639
Abhishek Dixit <47380596+abhid001@users.noreply.github.com>
2022-05-26 13:17:18 +0530

    Update 07 - almostIncreasingSequence.py

--- 07 - almostIncreasingSequence.py
+++ 07 - almostIncreasingSequence.py
@@ -18,7 +18,7 @@ solution(sequence) = true.
 You can remove 3 from the array to get the strictly increasing sequence [1, 2]. Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
 '''
 
-def first_bad_pair(sequence):
+def increasingSequence(sequence):
     """
     Return the first index of a pair of elements where the earlier
     element is not less than the later elements. If no such pair
@@ -29,17 +29,17 @@ def first_bad_pair(sequence):
             return i
     return -1
     
-def solution(sequence):
+def almostIncreasingSequence(sequence):
     """
     Return whether it is possible to obtain a strictly increasing
     sequence by removing no more than one element from the array.
     """
-    j = first_bad_pair(sequence)
+    j = increasingSequence(sequence)
     if j == -1:
         return True  # List is increasing
-    if first_bad_pair(sequence[j-1:j] + sequence[j+1:]) == -1:
+    if increasingSequence(sequence[j-1:j] + sequence[j+1:]) == -1:
         return True  # Deleting earlier element makes increasing
-    if first_bad_pair(sequence[j:j+1] + sequence[j+2:]) == -1:
+    if increasingSequence(sequence[j:j+1] + sequence[j+2:]) == -1:
         return True  # Deleting later element makes increasing
     return False  # Deleting either does not make increasing
 
