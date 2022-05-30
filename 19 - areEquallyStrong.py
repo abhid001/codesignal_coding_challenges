858774f9e382af4d8ced462cfae1aa3b2f4bc960
Abhishek Dixit <47380596+abhid001@users.noreply.github.com>
2022-05-30 16:37:55 +0530

    Create 19 - areEquallyStrong.py

---
+++ 19 - areEquallyStrong.py
@@ -0,0 +1,22 @@
+'''
+Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+
+Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left), and so are their weakest arms.
+
+Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+
+Example
+
+For yourLeft = 10, yourRight = 15, friendsLeft = 15, and friendsRight = 10, the output should be
+solution(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+For yourLeft = 15, yourRight = 10, friendsLeft = 15, and friendsRight = 10, the output should be
+solution(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+For yourLeft = 15, yourRight = 10, friendsLeft = 15, and friendsRight = 9, the output should be
+solution(yourLeft, yourRight, friendsLeft, friendsRight) = false.
+
+'''
+def solution(yourLeft, yourRight, friendsLeft, friendsRight):
+    if min(yourLeft,yourRight) == min(friendsLeft,friendsRight) and max(yourLeft,yourRight) == max(friendsLeft,friendsRight):
+        return True 
+    else:
+        return False
