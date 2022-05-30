0dfba86d7bc44876eee12a32c497926d4512e894
Abhishek Dixit <47380596+abhid001@users.noreply.github.com>
2022-05-30 16:26:41 +0530

    Update 18 - palindromeRearranging.py

--- 18 - palindromeRearranging.py
+++ 18 - palindromeRearranging.py
@@ -9,3 +9,5 @@ solution(inputString) = true.
 We can rearrange "aabb" to make "abba", which is a palindrome.
 
 '''
+def solution(inputString):
+    return sum([inputString.count(i)%2 for i in set(inputString)]) <= 1
